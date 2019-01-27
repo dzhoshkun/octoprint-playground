f6b6a9fc55896231450f3190a3dc8a1bdc770e32
Dzhoshkun Ismail Shakir <dzhoshkun.shakir@gmail.com>
2019-01-27 14:26:25 +0000

    inherit SettingsPlugin as well for URL setting

--- octoprint_mini/__init__.py
+++ octoprint_mini/__init__.py
@@ -1,13 +1,18 @@
 # coding=utf-8
 from __future__ import absolute_import
 
-from octoprint.plugin import StartupPlugin, TemplatePlugin
+from octoprint.plugin import (
+	StartupPlugin, TemplatePlugin, SettingsPlugin
+)
 
 
-class MiniPlugin(StartupPlugin, TemplatePlugin):
+class MiniPlugin(StartupPlugin, TemplatePlugin, SettingsPlugin):
 
 	def on_after_startup(self):
-		self._logger.info("Mini World!")
+		self._logger.info("Mini World! (more: %s)" % self._settings.get(["url"]))
+
+	def get_settings_defaults(self):
+		return dict(url="https://en.wikipedia.org/wiki/Hello_world")
 
 
 __plugin_implementation__ = MiniPlugin()
