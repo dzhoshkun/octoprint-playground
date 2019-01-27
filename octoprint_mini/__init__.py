0106e8b6b235e70215db567762496050bbdbda14
Dzhoshkun Ismail Shakir <dzhoshkun.shakir@gmail.com>
2019-01-27 14:07:53 +0000

    removed duplicates

--- octoprint_mini/__init__.py
+++ octoprint_mini/__init__.py
@@ -10,7 +10,4 @@ class MiniPlugin(octoprint.plugin.StartupPlugin):
 		self._logger.info("Mini World!")
 
 
-__plugin_name__ = "Mini Plugin"
-__plugin_version__ = "1.0.0"
-__plugin_description__ = "Mini plugin with minimal settings"
 __plugin_implementation__ = MiniPlugin()
