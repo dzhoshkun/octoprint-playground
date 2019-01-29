31470790302845ac467325cb12d8d2f8b2da191c
Dzhoshkun Ismail Shakir <dzhoshkun.shakir@gmail.com>
2019-01-29 19:44:55 +0000

    using BlueprintPlugin to define plugin API

--- octoprint_mini/__init__.py
+++ octoprint_mini/__init__.py
@@ -1,12 +1,23 @@
 # coding=utf-8
 from __future__ import absolute_import
 
+import flask
 from octoprint.plugin import (
-	StartupPlugin, TemplatePlugin, SettingsPlugin, ShutdownPlugin, AssetPlugin
+	StartupPlugin, TemplatePlugin, SettingsPlugin, ShutdownPlugin, AssetPlugin, BlueprintPlugin
 )
 
 
-class MiniPlugin(StartupPlugin, TemplatePlugin, SettingsPlugin, ShutdownPlugin, AssetPlugin):
+class MiniPlugin(StartupPlugin, TemplatePlugin, SettingsPlugin, ShutdownPlugin, AssetPlugin, BlueprintPlugin):
+
+	@BlueprintPlugin.route('/echo', methods=['GET'])
+	def my_echo(self):
+		print('DZHOSHKUN', 'got', flask.request.values)
+		if "text" not in flask.request.values:
+			return flask.make_response("Expected a text to echo back.", 400)
+		return flask.request.values["text"]
+
+	def is_blueprint_protected(self):
+		return False
 
 	def on_after_startup(self):
 		self._logger.info("Mini World! (more: %s)" % self._settings.get(["url"]))
